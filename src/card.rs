311ff1ba03c086b549e9cc3ec85a6110e19f14e4
Erik Feller <erik.feller@colorado.edu>
2022-02-18 14:42:42 -0700

    Initial deck & shuffle

---
+++ src/card.rs
@@ -0,0 +1,21 @@
+use std::fmt;
+
+#[derive(Debug)]
+pub struct Card {
+    pub suit: Suit,
+    pub value: u8
+}
+
+impl fmt::Display for Card {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.value)
+    }
+}
+
+#[derive(Debug)]
+pub enum Suit {
+    Hearts,
+    Spades,
+    Clubs,
+    Diamonds
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+mod card;
+
+use rand::thread_rng;
+use rand::seq::SliceRandom;
+
+use std::iter;
+use crate::card::Card;
+use crate::card::Suit;
+
 fn main() {
-    println!("Hello, world!");
+    let mut deck = Vec::new();
+    let values: [u8; 13]= [1,2,3,4,5,6,7,8,9,10,11,12,13];
+
+    for i in (0..12) {
+        for c in &values {
+            &deck.push(Card{suit:Suit::Hearts, value:*c});
+        }
+        if i%2 == 1 {
+            &deck.push(Card{suit:Suit::Hearts, value:13});
+        }
+    }
+
+    deck.shuffle(&mut thread_rng());
 }
